1cc9f3286ae552663a67983e02048201ad773a6a
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2024-04-11 10:43:56 +0200

    Generate bindings (missing include paths) for FEM

--- nrf-mpsl-sys/build.rs
+++ nrf-mpsl-sys/build.rs
@@ -59,6 +59,7 @@ fn bindgen(target: &Target) -> bindgen::Builder {
         .clang_arg("-I./third_party/arm/CMSIS_5/CMSIS/Core/Include")
         .clang_arg("-I./third_party/nordic/nrfx/mdk")
         .clang_arg("-I./third_party/nordic/nrfxlib/mpsl/include")
+        .clang_arg("-I./third_party/nordic/nrfxlib/mpsl/fem/include")
         .clang_arg(format!("-D{}", target.chip))
         .allowlist_function("mpsl_.*")
         .allowlist_function("MPSL_.*")
@@ -75,19 +76,51 @@ fn bindgen(target: &Target) -> bindgen::Builder {
 fn main() {
     let target = Target::new(env::var("TARGET").unwrap());
 
-    let fem = match (
+    let (fem_lib, fem_includes): (Option<&str>, Option<&[&str]>) = match (
         env::var_os("CARGO_FEATURE_FEM_SIMPLE_GPIO"),
         env::var_os("CARGO_FEATURE_FEM_NRF21540_GPIO"),
         env::var_os("CARGO_FEATURE_FEM_NRF21540_GPIO_SPI"),
     ) {
-        (None, None, None) => None,
-        (Some(_), None, None) => Some("simple_gpio"),
-        (None, Some(_), None) => Some("nrf21540_gpio"),
-        (None, None, Some(_)) => Some("nrf21540_gpio_spi"),
+        (None, None, None) => (None, None),
+        (Some(_), None, None) => (
+            Some("simple_gpio"),
+            Some(&[
+                "./third_party/nordic/nrfxlib/mpsl/fem/include/mpsl_fem_config_common.h",
+                "./third_party/nordic/nrfxlib/mpsl/fem/include/mpsl_fem_init.h",
+                "./third_party/nordic/nrfxlib/mpsl/fem/include/mpsl_fem_power_model.h",
+                "./third_party/nordic/nrfxlib/mpsl/fem/include/mpsl_fem_types.h",
+                "./third_party/nordic/nrfxlib/mpsl/fem/include/protocol/mpsl_fem_protocol_api.h",
+                "./third_party/nordic/nrfxlib/mpsl/fem/simple_gpio/include/mpsl_fem_config_simple_gpio.h",
+            ]),
+        ),
+        (None, Some(_), None) => (
+            Some("nrf21540_gpio"),
+            Some(&[
+                "./third_party/nordic/nrfxlib/mpsl/fem/include/mpsl_fem_config_common.h",
+                "./third_party/nordic/nrfxlib/mpsl/fem/include/mpsl_fem_init.h",
+                "./third_party/nordic/nrfxlib/mpsl/fem/include/mpsl_fem_power_model.h",
+                "./third_party/nordic/nrfxlib/mpsl/fem/include/mpsl_fem_types.h",
+                "./third_party/nordic/nrfxlib/mpsl/fem/include/mpsl_fem_config_nrf21540_common.h",
+                "./third_party/nordic/nrfxlib/mpsl/fem/include/protocol/mpsl_fem_protocol_api.h",
+                "./third_party/nordic/nrfxlib/mpsl/fem/nrf21540_gpio/include/mpsl_fem_config_nrf21540_gpio.h",
+            ]),
+        ),
+        (None, None, Some(_)) => (Some("nrf21540_gpio_spi"),
+            Some(&[
+                "./third_party/nordic/nrfxlib/mpsl/fem/include/mpsl_fem_config_common.h",
+                "./third_party/nordic/nrfxlib/mpsl/fem/include/mpsl_fem_init.h",
+                "./third_party/nordic/nrfxlib/mpsl/fem/include/mpsl_fem_power_model.h",
+                "./third_party/nordic/nrfxlib/mpsl/fem/include/mpsl_fem_types.h",
+                "./third_party/nordic/nrfxlib/mpsl/fem/include/mpsl_fem_config_nrf21540_common.h",
+                "./third_party/nordic/nrfxlib/mpsl/fem/include/protocol/mpsl_fem_protocol_api.h",
+                "./third_party/nordic/nrfxlib/mpsl/fem/nrf21540_gpio_spi/include/mpsl_fem_config_nrf21540_gpio_spi.h",
+                "./third_party/nordic/nrfxlib/mpsl/fem/nrf21540_gpio_spi/include/mpsl_fem_config_nrf21540_power_model_builtin.h",
+            ]),
+        ),
         _ => panic!("Only one front-end module feature may be enabled"),
     };
 
-    bindgen(&target)
+    let mut builder = bindgen(&target)
         .header("./include/stdlib.h")
         .header("./third_party/nordic/nrfxlib/mpsl/include/mpsl.h")
         .header("./third_party/nordic/nrfxlib/mpsl/include/mpsl_clock.h")
@@ -99,7 +132,15 @@ fn main() {
         .header("./third_party/nordic/nrfxlib/mpsl/include/mpsl_tx_power.h")
         .header("./third_party/nordic/nrfxlib/mpsl/include/nrf_errno.h")
         .header("./third_party/nordic/nrfxlib/mpsl/include/protocol/mpsl_cx_protocol_api.h")
-        .header("./third_party/nordic/nrfxlib/mpsl/include/protocol/mpsl_dppi_protocol_api.h")
+        .header("./third_party/nordic/nrfxlib/mpsl/include/protocol/mpsl_dppi_protocol_api.h");
+
+    if let Some(fem_includes) = fem_includes {
+        for include in fem_includes.iter() {
+            builder = builder.header(*include);
+        }
+    }
+
+    builder
         .generate()
         .expect("Unable to generate bindings")
         .write_to_file(PathBuf::from(env::var_os("OUT_DIR").unwrap()).join("bindings.rs"))
@@ -123,7 +164,7 @@ fn main() {
     println!("cargo:rustc-link-lib=static=mpsl");
     println!("cargo:rustc-link-lib=static=mpsl_fem_common");
 
-    if let Some(fem) = fem {
+    if let Some(fem) = fem_lib {
         let fem_lib_path = lib_path(format!("fem/{fem}"), &target);
         println!("cargo:rustc-link-search={}", fem_lib_path.to_str().unwrap());
         println!("cargo:rustc-link-lib=static=mpsl_fem_{}", fem);
